bcf115eb0a5a167a723117af0896e18614ec1c70
Yibo-Dong <53207925+Yibo-Dong@users.noreply.github.com>
2025-03-17 07:15:12 +0000

    Deploying to gh-pages from @ Yibo-Dong/sjtu019.github.io@99f7ef117eae9bba6c0b1e41b1cdcc40b9b3ef75 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "My Cool Projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -41,51 +41,36 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-automatic-correcting-of-basic-math-proofs",
+          title: 'Automatic Correcting of Basic Math Proofs',
+          description: "Automatic grading tool for mathematical proofs",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project.html";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/autocorrect.html";
+            },},{id: "projects-splitting-theory-of-c-programming-language",
+          title: 'Splitting Theory of C Programming Language',
+          description: "Decomposition theory for C programs",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project.html";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/decomposeC.html";
+            },},{id: "projects-an-annotated-variant-compcert-clightgen",
+          title: 'An Annotated variant CompCert-Clightgen',
+          description: "Extending CompCert for annotated C programs",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project.html";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+              window.location.href = "/projects/extendCompertC.html";
+            },},{id: "projects-formal-verification-of-interlocking-systems",
+          title: 'Formal Verification of Interlocking Systems',
+          description: "A formal framework for verifying railway interlocking systems",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project.html";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+              window.location.href = "/projects/lightf3.html";
+            },},{id: "projects-revisiting-literal-ordering-in-car-based-model-checking",
+          title: 'Revisiting Literal Ordering in CAR-Based Model Checking',
+          description: "Analyzing and extending CAR-based strategies",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project.html";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+              window.location.href = "/projects/literal_ordering.html";
+            },},{id: "projects-supercar",
+          title: 'SuperCAR',
+          description: "A parallel bit-level safety model checker",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project.html";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project.html";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project.html";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project.html";
+              window.location.href = "/projects/superCAR.html";
             },},{
         id: 'social-email',
         title: 'email',
